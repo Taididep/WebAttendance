4dc7c531c837a8b2f60c58041acb1cac7bf8fa8e
Taididep <dinhvantai079@gmail.com>
2024-10-21 10:44:40 +0700

    add file

--- Teacher/Class/class_create.php
+++ Teacher/Class/class_create.php
@@ -45,6 +45,7 @@ $semesters = $conn->query("SELECT * FROM semesters")->fetchAll(PDO::FETCH_ASSOC)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tạo lớp học</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css">
 </head>
 <body>
 <div class="container mt-5">
