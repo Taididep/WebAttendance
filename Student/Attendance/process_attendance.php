d7e39b255d212bc52c941280ec72ca75b217cbc0
Taididep <dinhvantai079@gmail.com>
2024-11-14 02:31:21 +0700

    update QR

--- Student/Attendance/process_attendance.php
+++ Student/Attendance/process_attendance.php
@@ -5,21 +5,19 @@ include __DIR__ . '/../../Connect/connect.php';
 include __DIR__ . '/../../LayoutPages/navbar.php';
 include __DIR__ . '/../../Account/islogin.php';
 
-// Kiểm tra xem class_id và schedule_id có được gửi qua URL hay không
-if (!isset($_POST['class_id']) || !isset($_POST['schedule_id'])) {
+// Kiểm tra xem class_id, schedule_id và status có được gửi qua POST hay không
+if (!isset($_POST['class_id']) || !isset($_POST['schedule_id']) || !isset($_POST['status'])) {
     echo 'Không tìm thấy thông tin lớp học hoặc lịch học.';
     exit;
 }
 
-// Lấy class_id và schedule_id từ POST
+// Lấy class_id, schedule_id và status từ POST
 $class_id = $_POST['class_id'];
 $schedule_id = $_POST['schedule_id'];
+$status = $_POST['status'];
 
 // Lấy user_id từ session
 $user_id = $_SESSION['user_id']; // Giả sử user_id được lưu trong session
-
-// Gán status là 1
-$status = 1;
 
 // Kiểm tra xem điểm danh đã tồn tại hay chưa
 $sqlCheck = "SELECT * FROM attendances WHERE schedule_id = ? AND student_id = ?";
